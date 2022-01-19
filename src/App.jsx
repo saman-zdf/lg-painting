39519e0ec342ec96b1bcbcd6463d4ceb001ef285
saman-zdf <samanfyz68@gmail.com>
2022-01-19 08:38:45 +0800

    added sidebar menu with links for all sections

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Works from './components/works/Works';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import './app.scss';
+import Menu from './components/menu/Menu';
 const App = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className='app'>
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className='sections'>
         <Intro />
         <Portfolio />
